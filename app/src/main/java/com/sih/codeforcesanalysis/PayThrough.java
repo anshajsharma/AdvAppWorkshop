29b50c5f94c88c80c9c78f4dcfb27710767bf167
ANSHAJ KUMAR <55790104+anshajsharma@users.noreply.github.com>
2020-03-20 00:08:06 +0530

    asdf

--- app/src/main/java/com/sih/codeforcesanalysis/PayThrough.java
+++ app/src/main/java/com/sih/codeforcesanalysis/PayThrough.java
@@ -3,6 +3,7 @@ package com.sih.codeforcesanalysis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,8 @@ public class PayThrough extends AppCompatActivity {
         setContentView(R.layout.activity_pay_through);
         // add any number of RadioButton resource IDs here
 
+
+
         cf=findViewById(R.id.cf);
         cf.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +28,29 @@ public class PayThrough extends AppCompatActivity {
                 startActivity(new Intent(PayThrough.this,MainActivity.class));
             }
         });
+
+
+        String GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
+        int GOOGLE_PAY_REQUEST_CODE = 123;
+
+        Uri uri =
+                new Uri.Builder()
+                        .scheme("upi")
+                        .authority("pay")
+                        .appendQueryParameter("pa", "your-merchant-vpa@xxx")
+                        .appendQueryParameter("pn", "your-merchant-name")
+                        .appendQueryParameter("mc", "your-merchant-code")
+                        .appendQueryParameter("tr", "your-transaction-ref-id")
+                        .appendQueryParameter("tn", "your-transaction-note")
+                        .appendQueryParameter("am", "your-order-amount")
+                        .appendQueryParameter("cu", "INR")
+                        .appendQueryParameter("url", "your-transaction-url")
+                        .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
+        intent.setPackage(GOOGLE_PAY_PACKAGE_NAME);
+        startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE);
+
 
 
     }
